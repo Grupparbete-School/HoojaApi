ffc870611ecc2ceff4c555c493dfeb36bd97ca0d
ullzten <ullzten@gmail.com>
2023-06-05 04:27:44 +0200

    fixxes

---
+++ HoojaApi/Models/DTO/User/UserPostDto.cs
@@ -0,0 +1,13 @@
+﻿namespace HoojaApi.Models.DTO.User
+{
+    public class UserPostDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string SecurityNumber { get; set; }
+        public string PasswordHash { get; set; }
+    }
+
+}
